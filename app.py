6856065238ec858dd6980ab03f8083dafe1e9be9
Sanabu W <sanabuwashizuka@gmail.com>
2021-04-04 20:12:29 -0700

    js connection check

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ Beach = create_classes(db)
 
 @app.route("/")
 def home():
-    results = db.session.query(Beach.beach_name, Beach.id).first()
+    results = db.session.query(Beach).first()
     print(results)
     return render_template("index.html", text1 = "jinja test1", text2 = results)
 
