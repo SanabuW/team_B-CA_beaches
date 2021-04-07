0f3bb390c0d525fab9d402811106f087aefe5f6c
Sanabu W <sanabuwashizuka@gmail.com>
2021-04-07 12:23:23 -0700

    changed datatypes to table_creator.sql

---
+++ working_files/dummy_table_creator.sql
@@ -0,0 +1,52 @@
+-- OLD BEACHES TABLE CREATION SCRIPT
+-- CREATE TABLE dates_test_table_beaches (	
+-- 	id SERIAL PRIMARY KEY,
+-- 	region VARCHAR,
+-- 	county VARCHAR,
+-- 	area VARCHAR,
+-- 	beach_name VARCHAR,
+-- 	beach_url VARCHAR,
+-- 	address VARCHAR,
+-- 	city VARCHAR,
+-- 	state VARCHAR,
+-- 	zip VARCHAR,
+-- 	latitude FLOAT,
+-- 	longitude FLOAT,
+-- 	park_name VARCHAR,
+-- 	owner VARCHAR,
+-- 	owner_url VARCHAR,
+-- 	activities VARCHAR,
+-- 	amenities VARCHAR,
+-- 	pet_policy VARCHAR,
+-- 	pets_allowed VARCHAR,
+-- 	fees VARCHAR,
+-- 	free_parking VARCHAR,
+-- 	phone VARCHAR,
+-- 	other_names VARCHAR
+-- );
+
+
+
+CREATE TABLE dates_test_table_grades (	
+	id SERIAL PRIMARY KEY,
+	json_id INT,
+	title VARCHAR,
+	name1 VARCHAR,
+	latitude FLOAT,
+	longitude FLOAT,
+	address VARCHAR,
+	city VARCHAR,
+	county VARCHAR,
+	state VARCHAR,
+	zip VARCHAR,
+	active VARCHAR,
+	grade_updated DATE,
+	dry_grade VARCHAR,
+	wet_grade VARCHAR,
+	annual_summer_dry VARCHAR,
+	annual_year_wet VARCHAR,
+	annual_winter_dry VARCHAR,
+	annual_year VARCHAR,
+	grade_created DATE,
+	alerts VARCHAR
+);
